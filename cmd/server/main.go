3139499eafa4fc30464c72e5c9dabb5f66e10ae2
CybrRonin <APStories@gmail.com>
2026-03-12 19:05:50 -0400

    server app connects to RabbitMQ and waits for shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection successful!")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Server shutting down...")
 }
