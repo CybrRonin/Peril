11ec37c1d1f776d0fb648b7b19ca2e7e499be7d5
CybrRonin <APStories@gmail.com>
2026-03-13 17:17:16 -0400

    updated server to handle commands

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -26,15 +27,32 @@ func main() {
 		log.Fatalf("Failed to create channel: %v", err)
 	}
 
-	data := routing.PlayingState{
-		IsPaused: true,
-	}
-	err = pubsub.PublishJSON(publishChan, routing.ExchangePerilDirect, routing.PauseKey, data)
-	if err != nil {
-		log.Fatalf("Error publishing JSON: %v", err)
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			fmt.Println("Sending pause message...")
+			setPause(publishChan, true)
+		case "resume":
+			fmt.Println("Sending resume message...")
+			setPause(publishChan, false)
+		case "quit":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Printf("Invalid command!")
+		}
 	}
 
-	fmt.Println("Pause message sent!")
+	/*
+
+	 */
 	/*
 	   // wait for ctrl+c
 	   signalChan := make(chan os.Signal, 1)
@@ -43,4 +61,15 @@ func main() {
 
 	   fmt.Println("Server shutting down...")
 	*/
+}
+
+func setPause(publishChan *amqp.Channel, paused bool) {
+
+	data := routing.PlayingState{
+		IsPaused: paused,
+	}
+	err := pubsub.PublishJSON(publishChan, routing.ExchangePerilDirect, routing.PauseKey, data)
+	if err != nil {
+		log.Fatalf("could not publish time: %v", err)
+	}
 }
